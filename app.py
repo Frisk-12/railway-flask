a73d310d1d97806b42d4d4a659de3616962b738c
Frisk-12 <126598650+Frisk-12@users.noreply.github.com>
2025-02-23 00:29:46 +0100

    Update app.py
    cambiato in 8000 la porta

--- app.py
+++ app.py
@@ -133,4 +133,4 @@ def handler(event, context):
 # Main: esecuzione dell'app (Vercel utilizzerà direttamente 'app')
 # ===============================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
